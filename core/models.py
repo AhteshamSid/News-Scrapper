1e7ae0374536b511eb13a506e4ec311212cf6da1
AhteshamSid <78630600+AhteshamSid@users.noreply.github.com>
2021-08-22 14:14:42 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,28 +1,16 @@
 from django.db import models
 
 portals = (
-    ('Benchmark.pl', 'Benchmark.pl'),
-    ('boardgamesgeek.com', 'boardgamesgeek.com'),
-    ('Tojuzbylo.pl', 'Tojuzbylo.pl'),
-    ('Zwiadowca Historii', 'Zwiadowca Historii'),
-    ('Computer World', 'Computer World'),
-    ('InfoWorldPython.com', 'InfoWorldPython.com'),
-    ('RealPython.com', 'RealPython.com'),
-    ('bushcraftable.com', 'bushcraftable.com'),
     ('ndtv.com', 'ndtv.com'),
     ('soundcloud.com', 'soundcloud.com'),
 )
 
-languages = (
-    ('PL', 'PL'),
-    ('ENG', 'ENG'),
-)
+
 
 
 class Article(models.Model):
     portal = models.CharField(max_length=100, choices=portals)
     title = models.CharField(max_length=256)
-    language = models.CharField(max_length=3, choices=languages)
     date_created = models.DateTimeField(auto_now_add=True)
     url = models.URLField()
 
@@ -30,7 +18,7 @@ class Article(models.Model):
         return f'{self.portal} - {self.title}'
 
     @classmethod
-    def check_if_article_already_exist(cls, posts_list, portal, language) -> None:
+    def check_if_article_already_exist(cls, posts_list, portal) -> None:
         all_articles = Article.objects.filter(portal=portal)
         article_list = []
         [article_list.append(article.url) for article in all_articles]
@@ -39,17 +27,16 @@ class Article(models.Model):
             if post[0] in article_list:
                 continue
             else:
-                cls.save_article(post[1], post[0], portal, language)
+                cls.save_article(post[1], post[0], portal)
 
     @classmethod
-    def save_article(cls, title, url, portal, language) -> None:
+    def save_article(cls, title, url, portal) -> None:
         if title == '' or url == '':
             pass
         else:
             article = Article(
                 portal=portal,
                 title=title,
-                language=language,
                 url=url,
             )
             article.save()
