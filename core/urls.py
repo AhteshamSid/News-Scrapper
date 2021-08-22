7c255812adfa6bb717cec602988a1e65527d0d62
AhteshamSid <78630600+AhteshamSid@users.noreply.github.com>
2021-08-22 14:18:14 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import PythonView, RealPythonView, HomeView, BenchmarkView, BoardGamesGeekView, ArcheologyView, \
-    ToJuzByloView, ComputerWorldView, BushcraftableView, StatisticsView, NdtvView, QtvView
+from .views import  HomeView, StatisticsView, NdtvView, QtvView
 
 app_name = 'core'
 
@@ -8,15 +7,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('ndtv/', NdtvView.as_view(), name="ndtv"),
     path('qtv/', QtvView.as_view(), name="qtv"),
-    path('benchmark/', BenchmarkView.as_view(), name="benchmark"),
-    path('bgg/', BoardGamesGeekView.as_view(), name="bgg"),
-    path('archeology/', ArcheologyView.as_view(), name="archeology"),
-    path('tojuzbylo/', ToJuzByloView.as_view(), name="tojuzbylo"),
-    path('computerworld/', ComputerWorldView.as_view(), name="computer-world"),
-    path('python/', PythonView.as_view(), name="python"),
-    path('real-python/', RealPythonView.as_view(), name="real-python"),
-    path('bushcraftable/', BushcraftableView.as_view(), name="bushcraftable"),
-    path('statistics/', StatisticsView.as_view(), name="statistics"),
 
     path('chart-1/', StatisticsView.get_all_article_pie_chart_data, name='chart-1'),
     path('chart-2/', StatisticsView.get_all_article_tab_chart_data, name='chart-2'),
